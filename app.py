9929211723e3fa6ff2f5396f1b604bc9602193ef
jessicamensah <76847649+jessicamensah@users.noreply.github.com>
2024-08-12 06:24:10 +0100

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import gradio as gr
-from MultiAgent import travel_convo
+from MultiAgent import TravelAssistant
+from data_handler import Data_Handler
 
-travel_convo_instance = travel_convo()
+travel_convo_instance = TravelAssistant()
 
 def multiagent_convo(message, history):
     """
@@ -14,22 +15,23 @@ def multiagent_convo(message, history):
     Returns:
         tuple: A tuple containing an empty string and the updated history
     """
-    bot_message = travel_convo_instance.find_travel_chat(message, history)
-    history.append((message, bot_message))
+    bot_message = travel_convo_instance.travel_convo(message, history)
+    if history is not None:
+        history.append((message, bot_message))
     return "", history
 
+# message = "id like to go to Rome , find a nice hotel"
+# chat_history = [[]]
+# print(multiagent_convo(message, chat_history))
 
-# Create chatbot using Gradio
+
+# create chatbot using gradio
 with gr.Blocks() as demo:
-    chatbot = gr.Chatbot(value=[["", "Hi! I'm your Travel Itinerary Bot, where would you like to go?"]])
-    msg = gr.Textbox(show_label=False, placeholder="Enter your message here...")
-    clear = gr.Button("Clear")
-
-    def clear_chat():
-        return gr.update(value=[]), gr.update(value="")
+    chatbot = gr.Chatbot(value=[["", "Where would you like to go?"]])
+    msg = gr.Textbox()
+    clear = gr.ClearButton([msg, chatbot])
 
     msg.submit(multiagent_convo, [msg, chatbot], [msg, chatbot], queue=False)
-    clear.click(clear_chat, [], [chatbot, msg])
 
-demo.launch(
-server_port=8080)
+demo.launch(share=True,
+    server_port=8080)
